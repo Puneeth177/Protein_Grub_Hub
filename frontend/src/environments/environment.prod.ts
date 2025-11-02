9cd7b9a0530a6e3c312bb67d910f8f331a8f0e12
08SAI <saichiragb8@gmail.com>
2025-11-02 10:30:50 +0530

    02-11-2025

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   apiUrl: 'https://protein-grub-hub.onrender.com/api',
   googleClientId: '425797682801-fiqcsfg9pktesvn25e2ihpnbevsvlpf6.apps.googleusercontent.com',
-  appUrl: 'https://protein-grub-hub-q3dn.onrender.com',
+  appUrl: 'https://pgh-frontend.onrender.com/',
   avatarApiEnabled: false,
   stripePublishableKey: 'pk_test_51SNZTQRuGCdDzhLHBatpmbyYwZFRgq9h5fAUcrT6sqD8kxKZTbkLGROtapnSlRWXL1rMEPYt0k6dXlgu5gFLXWwI00AqPuhRvQ'
 };
