4a6a6b200d70a8802d5ce3f54df092e136b9a35c
08SAI <saichiragb8@gmail.com>
2025-11-07 17:42:52 +0530

    07-11-2025

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api',
+  apiUrl: 'https://protein-grub-hub.onrender.com/api',
   googleClientId: '425797682801-fiqcsfg9pktesvn25e2ihpnbevsvlpf6.apps.googleusercontent.com',
   appUrl: 'http://localhost:4201',  // Updated Angular app URL
   avatarApiEnabled: false,
